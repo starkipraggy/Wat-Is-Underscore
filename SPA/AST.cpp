38d6b993368cb333d8480209002c12cd994199d6
ongzlalan <ongzl.alan@gmail.com>
2016-02-14 15:56:38 +0800

    added children and parent functions

--- SPA/AST.cpp
+++ SPA/AST.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include "AST.h"
+#include "Tnode.h"
 
 
 
@@ -23,4 +24,37 @@ TNode* AST::findNode(TNode* node) {
 			return tree[i];
 		}
 	}
+	return new TNode();
+}
+
+void AST::addToTree(TNode* node) {
+	tree.push_back(node);
+}
+
+void AST::makeChild(TNode* parent, TNode* child) {
+	for (unsigned i = 0; i < tree.size(); i++) {
+		if (parent == tree[i]) {
+			tree[i]->addChild(child);
+			child->setParent(tree[i]);
+		}
+	}
+}
+
+void AST::makeParent(TNode* child, TNode* parent) {
+	for (unsigned i = 0; i < tree.size(); i++) {
+		if (child == tree[i]) {
+			tree[i]->setParent(parent);
+			parent->addChild(tree[i]);
+		}
+	}
+}
+
+std::vector<TNode* > AST::getChildren(TNode* node) {
+	TNode* parentNode = findNode(node);
+	return node->getChildNodes();
+}
+
+TNode* AST::getParent(TNode* node) {
+	TNode* childNode = findNode(node);
+	return node->getParent();
 }
--- SPA/AST.h
+++ SPA/AST.h
@@ -12,5 +12,10 @@ public:
 	~AST();
 	std::vector<TNode*> getTree();
 	TNode* findNode(TNode* node);
+	void addToTree(TNode* node);
+	void makeChild(TNode* parent, TNode* child);
+	std::vector<TNode*> getChildren(TNode * node);
+	TNode * getParent(TNode * node);
+	void makeParent(TNode* child, TNode* parent);
 };
 
