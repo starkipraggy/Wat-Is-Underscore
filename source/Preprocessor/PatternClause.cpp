f0425a3f82cf4362e4b3f62f6338f3bacb90083f
tanch88 <chunhow88@hotmail.com>
2016-02-17 19:18:19 +0800

    add .equals
    for testing

--- source/Preprocessor/PatternClause.cpp
+++ source/Preprocessor/PatternClause.cpp
@@ -12,3 +12,18 @@ Variable PatternClause::getAssignedVariable() {
 string PatternClause::getQuery() {
 	return Clause::getQuery() + "+" + assignedVariable.toString();
 }
+
+bool PatternClause::equals(Clause* c) {
+	bool result;
+	if (PatternClause* p = dynamic_cast<PatternClause*>(c)){
+		result = clause == p->getClause()
+			&& variableOne.equals(p->getVariableOne())
+			&& variableTwo.equals(p->getVariableTwo())
+			&& assignedVariable.equals(p->getAssignedVariable());
+	}
+	else {
+		result = false;
+	}
+
+	return result;
+}
