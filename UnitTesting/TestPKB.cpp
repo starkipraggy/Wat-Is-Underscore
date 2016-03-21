1883b5a3af0533a027676e28f1fe937ab284b114
Foleosy <mywong@outlook.sg>
2016-03-21 17:42:14 +0800

    Updated TestPKB with Functioning Assignments

--- UnitTesting/TestPKB.cpp
+++ UnitTesting/TestPKB.cpp
@@ -15,13 +15,26 @@ namespace UnitTesting
         std::vector<std::string> tokens;
         std::vector<ExpressionTokenType> types;
 
-        TEST_METHOD(TestPKB_Assignment_01) { 
-            PKB::getInstance()->ProcedureStart("firstTestProc");
+        TEST_METHOD(TestPKB_Valid_Assignment) {
+            PKB::getInstance()->ProcedureStart("validProc");
 
+            // Valid Assignment Statement
             varName = "a";
-            tokens = { "a", "=", "1", ";" };
+            tokens = { "a", "=", "1" };
             types = { Variable, Operator, Constant };
             Assert::IsTrue(PKB::getInstance()->AssignStatement(varName, tokens, types));
+
+            PKB::getInstance()->ProcedureEnd();
+        }
+
+        TEST_METHOD(TestPKB_Invalid_Assignment) {
+            PKB::getInstance()->ProcedureStart("validProc");
+
+            // Invalid Assignment, no Right-Hand Expression
+            varName = "a";
+            tokens = { "a", "=" };
+            types = { Variable, Operator, Constant };
+            Assert::IsFalse(PKB::getInstance()->AssignStatement(varName, tokens, types));
 
             PKB::getInstance()->ProcedureEnd();
         }
