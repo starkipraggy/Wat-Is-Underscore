40202ffbe5bf4a5cf8b5ae7318a47340dbe1eafa
Foleosy <mywong@outlook.sg>
2016-03-21 17:12:59 +0800

    Updated TestPKB with First Assignment

--- UnitTesting/TestPKB.cpp
+++ UnitTesting/TestPKB.cpp
@@ -15,55 +15,16 @@ namespace UnitTesting
         std::vector<std::string> tokens;
         std::vector<ExpressionTokenType> types;
 
-        TEST_METHOD_INITIALIZE(InitialisePKB) {
+        TEST_METHOD(TestPKB_Assignment_01) { 
             PKB::getInstance()->ProcedureStart("firstTestProc");
 
             varName = "a";
             tokens = { "a", "=", "1", ";" };
             types = { Variable, Operator, Constant };
-            PKB::getInstance()->AssignStatement(varName, tokens, types);
+            Assert::IsTrue(PKB::getInstance()->AssignStatement(varName, tokens, types));
 
             PKB::getInstance()->ProcedureEnd();
         }
 
-        TEST_METHOD(TestPKB_ProcedureTableProcedure) {
-           
-            //Assert
-        } 
-        
-        TEST_METHOD(TestPKB_ProcedureTable) {
-            int actualNumProc = PKB::getInstance()->procedureTable.getNumberOfProcedures();
-            int expectedNumProc = 1;
-            
-            std::string actualProcName = PKB::getInstance()->procedureTable.getProcedure(1);
-            std::string expectedProcName = "firstTestProc";
-            
-            //Assert
-            Assert::IsTrue(actualNumProc == expectedNumProc);
-            Assert::IsTrue(actualProcName == expectedProcName);
-        }
-        
-        TEST_METHOD(TestPKB_StatementTableStatement) {
-           
-            //Assert
-        }
-        
-        TEST_METHOD(TestPKB_StatementTable) {
-            int actualNumStatements = PKB::getInstance()->statementTable.getNumberOfStatements();
-            int expectedNumStatements = 1;
-
-            //Assert
-            Assert::IsTrue(actualNumStatements == expectedNumStatements);
-        }
-
-        TEST_METHOD(TestPKB_VariableTableVariable) {
-           
-            //Assert
-        }
-
-        TEST_METHOD(TestPKB_VariableTable) {
-            
-            //Assert
-        }
     };
 }
