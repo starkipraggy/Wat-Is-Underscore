86634daeb20fe38271027addc11a2e2807a383d6
Foleosy <mywong@outlook.sg>
2016-03-24 07:34:21 +0800

    Added basic StmtTable test

--- UnitTesting/TestTables.cpp
+++ UnitTesting/TestTables.cpp
@@ -48,20 +48,90 @@ namespace UnitTesting
             Assert::AreEqual(1, newProcedureTable.getNumberOfProcedures());
             Assert::AreEqual(testProcName, newProcedureTable.getProcedure(testProcName)->getName());
             Assert::AreNotEqual(testProcName, newProcedureTable.getProcedure("testProcName2")->getName());
-          //Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getIndex());
+            //Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getIndex());
             Assert::AreEqual(0, newProcedureTable.getProcedure(testProcName)->getProcedureCallsSize());
             Assert::AreEqual(0, newProcedureTable.getProcedure(testProcName)->getStatementCallsSize());
             Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getUsesSize());
-          //Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getUses(1));
+            //Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getUses(1));
             Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getModifiesSize());
-          //Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getModifies(1));
+            //Assert::AreEqual(1, newProcedureTable.getProcedure(testProcName)->getModifies(1));
 
         }
 
-       
+        TEST_METHOD(TestPKB_StmtTable) {
+            ProcedureTable newProcedureTable;
+            StatementTable newStatementTable;
+            VariableTable newVariableTable;
 
-        TEST_METHOD(TestPKB_TableInitialize) {
-           
+            std::string testProcName = "testProc01";
+            ProcedureTableProcedure::ProcedureTableProcedure(testProcName, 1);
+            newProcedureTable.getProcedure(testProcName);
+
+            StatementTableStatement::StatementTableStatement(1);
+            newStatementTable.addStatement(1);
+            newProcedureTable.getProcedure(testProcName)->addStatement(1);
+
+            NAME a;
+            VariableTableVariable::VariableTableVariable(a, 1);
+            newVariableTable.getVariableUsingName(a);
+
+            newProcedureTable.getProcedure(testProcName)->addStatement(1);
+            Assert::IsTrue(newProcedureTable.getProcedure(testProcName)->addModifies(1));
+            Assert::IsTrue(newProcedureTable.getProcedure(testProcName)->addUses(1));
+
+            Assert::IsTrue(newStatementTable.getStatementUsingStatementNumber(1)->addModifies(1));
+            Assert::IsTrue(newStatementTable.getStatementUsingStatementNumber(1)->addUses(1));
+
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addProcedureModifies(1));
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addProcedureUses(1));
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addStatementModifies(1));
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addStatementUses(1));
+
+            Assert::AreEqual(1, newStatementTable.getNumberOfStatements());
+            Assert::AreEqual(1, newStatementTable.getStatementUsingStatementNumber(1)->getStatementNumber());
+            Assert::IsFalse(newStatementTable.getStatementUsingStatementNumber(1)->hasFollows());
+            Assert::IsFalse(newStatementTable.getStatementUsingStatementNumber(1)->hasFollowedBy());
+            Assert::IsFalse(newStatementTable.getStatementUsingStatementNumber(1)->hasParent());
+            //Assert::AreEqual(1, newStatementTable.getStatementUsingStatementNumber(1)->getUses(1));
+            Assert::AreEqual(1, newStatementTable.getStatementUsingStatementNumber(1)->getUsesSize());
+            //Assert::AreEqual(1, newStatementTable.getStatementUsingStatementNumber(1)->getModifies(1));
+            Assert::AreEqual(1, newStatementTable.getStatementUsingStatementNumber(1)->getModifiesSize());
+            Assert::AreEqual(0, newStatementTable.getStatementUsingStatementNumber(1)->getFollowsStarSize());
+            Assert::AreEqual(0, newStatementTable.getStatementUsingStatementNumber(1)->getFollowedByStarSize());
+            Assert::AreEqual(0, newStatementTable.getStatementUsingStatementNumber(1)->getChildrenSize());
+            Assert::AreEqual(0, newStatementTable.getStatementUsingStatementNumber(1)->getParentStarSize());
+
+        }
+
+        TEST_METHOD(TestPKB_VarTable) {
+            ProcedureTable newProcedureTable;
+            StatementTable newStatementTable;
+            VariableTable newVariableTable;
+
+            std::string testProcName = "testProc01";
+            ProcedureTableProcedure::ProcedureTableProcedure(testProcName, 1);
+            newProcedureTable.getProcedure(testProcName);
+
+            StatementTableStatement::StatementTableStatement(1);
+            newStatementTable.addStatement(1);
+            newProcedureTable.getProcedure(testProcName)->addStatement(1);
+
+            NAME a;
+            VariableTableVariable::VariableTableVariable(a, 1);
+            newVariableTable.getVariableUsingName(a);
+
+            newProcedureTable.getProcedure(testProcName)->addStatement(1);
+            Assert::IsTrue(newProcedureTable.getProcedure(testProcName)->addModifies(1));
+            Assert::IsTrue(newProcedureTable.getProcedure(testProcName)->addUses(1));
+
+            Assert::IsTrue(newStatementTable.getStatementUsingStatementNumber(1)->addModifies(1));
+            Assert::IsTrue(newStatementTable.getStatementUsingStatementNumber(1)->addUses(1));
+
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addProcedureModifies(1));
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addProcedureUses(1));
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addStatementModifies(1));
+            Assert::IsTrue(newVariableTable.getVariableUsingName(a)->addStatementUses(1));
+
         }
     };
 }
