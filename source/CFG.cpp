715ded8233225ce32399710cd99b3739c1c4b18b
starkipraggy <starkipraggy@hotmail.com>
2016-04-04 17:15:55 +0800

    CFG fix for if nested in while redux

--- source/CFG.cpp
+++ source/CFG.cpp
@@ -175,7 +175,13 @@ std::vector<int> CFG::prevStmt(int stmtNum){
         result.push_back(stmtNum - 1);
     } else {
         for (size_t i = 0; i < thisNode->getParents().size(); i++) {
-            result.push_back(thisNode->getParents()[i]->getRightLmt());
+            if (thisNode->getParents()[i]->getType() == Unused) {
+                for (size_t j = 0; j < thisNode->getParents()[i]->getParents().size(); j++) {
+                    result.push_back(thisNode->getParents()[i]->getParents()[j]->getRightLmt());
+                }
+            } else {
+                result.push_back(thisNode->getParents()[i]->getRightLmt());
+            }
         }
     }
     return result;
@@ -187,10 +193,10 @@ std::vector<int> CFG::nextStmt(int stmtNum){
     if (stmtNum < thisNode->getRightLmt()) {
         result.push_back(stmtNum + 1);
     } else {
-        if (thisNode->getChd1() != NULL ){
+        if (thisNode->getChd1() != NULL && thisNode->getChd1()->getType() != Unused){
             result.push_back(thisNode->getChd1()->getLeftLmt());
         }
-        if (thisNode->getChd2() != NULL ) {
+        if (thisNode->getChd2() != NULL && thisNode->getChd2()->getType() != Unused) {
             result.push_back(thisNode->getChd2()->getLeftLmt());
         }
     }
