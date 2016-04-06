d27e573ab9c71fafd6732600db2f4a907681dff6
Chua Wei Liang <a0119754@nus.edu.sg>
2016-04-06 11:23:04 +0800

    Made the Next and Next* functions consistent in terms of infrastructure

--- source/StatementTableStatement.h
+++ source/StatementTableStatement.h
@@ -391,7 +391,7 @@ public:
 		use this function to retrieve a vector of pointers to statements s which Next*(s, this) is true
 		\return List of pointers to statements that this statement has this statement after directly or indirectly in the CFG
 	*/
-	std::vector<StatementTableStatement*> getPreviousStar();
+	std::vector<StatementTableStatement*>* getPreviousStar();
 
 	//! Getter function for the list of pointers to statements that comes after this statement directly or indirectly in the CFG
 	/*!
@@ -399,7 +399,7 @@ public:
 		use this function to retrieve a vector of pointers to statements s which Next*(this, s) is true
 		\return List of pointers to statements that comes after this statement directly or indirectly in the CFG
 	*/
-	std::vector<StatementTableStatement*> getNextStar();
+	std::vector<StatementTableStatement*>* getNextStar();
 
 	//! Getter function for the list of pointers to statements that directly affects this statement
 	/*!
